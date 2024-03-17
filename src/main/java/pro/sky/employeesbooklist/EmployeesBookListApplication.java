219b4e59e5200551c67c95ef81d23a9e3ad7a734
latsa <latsapnev01@mail.ru>
2024-03-17 20:01:36 +0300

    Создал spring boot проект.

---
+++ src/main/java/pro/sky/employeesbooklist/EmployeesBookListApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeesbooklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesBookListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesBookListApplication.class, args);
+	}
+
+}
